ac6b464ae84fb4b700926076cf858fd0c0b269f3
PolizerBruno <60673615+PolizerBruno@users.noreply.github.com>
2020-09-16 09:46:37 -0300

    authScreen com register

--- src/screen/auth.js
+++ src/screen/auth.js
@@ -7,8 +7,10 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 export default class extends Component {
 
     state = {
+        name : '',
         email : '',
         password : '',
+        screenRegister : false,
     }
 
     render () {
@@ -16,17 +18,32 @@ export default class extends Component {
             <View style={Style.BackGround}>
                 <ImageBackground style={Style.Container} source={imageBack} />
                 <View style={Style.LoginForm}>
+                    {this.state.screenRegister && 
+                        <TextInput value={this.state.name} onChange={name=>this.setState({name})} placeholder={"Name"} style={Style.Input} />
+                    }
                     <TextInput value={this.state.email} onChange={email=>this.setState({email})} placeholder={"Email"} style={Style.Input} />
                     <TextInput value={this.state.password} onChange={password=>this.setState({password})} placeholder={"Password"} secureTextEntry={true} style={Style.Input} />
+                    {this.state.screenRegister ?  
+                    <TouchableHighlight style={Style.LoginButton}>
+                        <Text style={Style.LoginText}>Register</Text>
+                    </TouchableHighlight> : 
                     <TouchableHighlight style={Style.LoginButton}>
                         <Text style={Style.LoginText}>Login</Text>
-                     </TouchableHighlight>
+                    </TouchableHighlight>
+                    }
+                    {this.state.screenRegister ? false : 
                      <TouchableHighlight style={Style.RecoverPass}>
                         <Text style={Style.RecoverText}>Forgot Password/Email ?</Text>
                      </TouchableHighlight>
-                     <TouchableHighlight style={Style.registerButton}>
+                    }
+                    {this.state.screenRegister ?  
+                    <TouchableHighlight onPress={()=>this.setState({screenRegister : !this.state.screenRegister})} style={Style.registerButton}>
+                        <Text style={Style.RegisterText}>Back <Icon  name="arrow-circle-left" size={15} color="#000"/></Text>
+                     </TouchableHighlight> : 
+                     <TouchableHighlight onPress={()=>this.setState({screenRegister : !this.state.screenRegister})} style={Style.registerButton}>
                         <Text style={Style.RegisterText}>Create your account <Icon  name="arrow-circle-right" size={15} color="#000"/></Text>
                      </TouchableHighlight>
+                    }
                 </View>
             </View>
         
